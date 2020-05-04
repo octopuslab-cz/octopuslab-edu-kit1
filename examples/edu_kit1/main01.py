f1638898485d9edf2fa879064545fc50b0eca4a1
Honza S. Copak <bonum.comunae@volny.cz>
2020-05-04 11:56:29 +0200

    add disp7pause and printOctopus logo

--- examples/edu_kit1/main01.py
+++ examples/edu_kit1/main01.py
@@ -4,11 +4,11 @@
 from time import sleep
 from util.led import Led
 from util.rgb import Rgb
-from util.octopus import disp7_init
+from util.octopus import disp7_init, printOctopus
 from util.pinout import set_pinout
 
 print("this is simple Micropython example | ESP32 & octopusLAB")
-
+printOctopus()
 pinout = set_pinout()  # set board pinout
 from util.io_config import get_from_file
 io_conf = get_from_file()
@@ -21,7 +21,17 @@ ws.simpleTest()
 
 d7 = disp7_init()  # 7segment (8digit) display init
 
+def d7pause(ch = "-", sl = 0.5):
+    for i in range(8):
+        d7.show(" "*i + ch)
+        sleep(sl)
+
 sleep(2)
+d7pause(sl=0.1)
+
+ws.rainbow_cycle()
+ws.color((0,0,0))
+
 for i in range(9):
     d7.show(10-i)
     sleep(0.3)
